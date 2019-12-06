367e0b7ed284a4f8df039feef5ac5722fd03a670
Fadhil-Sabar <58591531+Fadhil-Sabar@users.noreply.github.com>
2019-12-06 17:26:49 +0700

    Add files via upload

---
+++ latihan_1/index.php
@@ -0,0 +1,146 @@
+<?php require_once "include/navbar.html" ?>
+<div class="jumbotron jumbotron-fluid mt-5">
+  <div class="container text-center">
+  	<img width="35%" class="rounded-circle img-thumbnail" src="assets/images/about.jpg">
+    <h1 class="display-4">Fadhil Andriawan</h1>
+    <p class="lead">Manusia biasa</p>
+  </div>
+</div>
+
+<section class="about" id="about">
+<div class="container ">
+	<br>
+	<div class="row mb-4 pt-5 mt-5">
+		<div class="col text-center">
+			<h2>About</h2>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col text-justify">
+			<p>Lahir di Jakarta, hari Senin, 14 Juli 2003 di RS Fatmawati. dan sekarang berusia 16 tahun. TK di Al-Muthaharun, SD di SDN Sawah Baru II, SMP di SMPN 6 Tangsel, dan saat ini SMK di SMK Negeri 6 Tangsel jurusan Rekayasa Perangkat Lunak
+		</p>
+		</div>
+		<div class="col text-justify">
+			<p>Hobi saya saat ini adalah berlatih Tae Kwon Do di Ekadasa Taekwondo Club. Saat ini saya mencapai sabuk merah, dan telah meraih beberapa penghargaan. Meski begitu saya tidak memakai nya untuk tindakan kekerasan. Berbeda dengan yang sebelumnya, saya di SMK ini tidak terlalu aktif.</p>
+		</div>
+	</div>
+</div>
+</section>
+
+
+<section class="kehidupan bg-light pb-4" id="kehidupan">
+<div class="container ">
+	<br>
+	<div class="row mb-4 pt-5 mt-5">
+		<div class="col text-center">
+			<h2>Kehidupan</h2>
+		</div>
+	</div>
+	<div class="row mb-4">
+		<div class="col-md">
+			<div class="card border-primary" style="width: 18rem;">
+			  <img style="max-height: 300px" class="card-img-top" src="assets/images/thunder.jpg" alt="tunderr">
+			  <div class="card-body">
+			    <p class="card-text">Me with my Thunder</p>
+
+			    <p class="card-text">-12/8/2019</p>
+			  </div>
+			</div>
+		</div>
+		<div class="col-md">
+			<div class="card border-primary" style="width: 18rem;">
+			  <img class="card-img-top" src="assets/images/ekadasa.jpg" alt="ekadasa">
+			  <div class="card-body">
+			    <p class="card-text">Ekadasa Taekwondo Club</p>
+			    <p class="card-text">since 2019</p>
+			  </div>
+			</div>
+		</div>
+		<div class="col-md">
+			<div class="card border-primary" style="width: 18rem;">
+			  <img class="card-img-top" src="assets/images/rover.jpg" alt="rover meet">
+			  <div class="card-body">
+			    <p class="card-text">Rover Meet Setu Parigi</p>
+
+			    <p class="card-text">-24 December 2018</p>
+			  </div>
+			</div>
+		</div>
+	</div>
+	<div class="row mb-5">
+		<div class="col-md">
+			<div class="card border-primary" style="width: 18rem;">
+			  <img class="card-img-top" src="assets/images/jarak.jpg" alt="turing">
+			  <div class="card-body">
+			    <p class="card-text">About 1400km++ touring</p>
+			    <p class="card-text">-1 Juni 2019</p>
+			  </div>
+			</div>
+		</div>
+		<div class="col-md">
+			<div class="card border-primary" style="width: 18rem;">
+			  <img class="card-img-top" src="assets/images/sauna.jpg" alt="sauna">
+			  <div class="card-body">
+			    <p class="card-text">The Sauna's</p>
+			    <p class="card-text">since 24 March 2019</p>
+			  </div>
+			</div>
+		</div>
+		<div class="col-md">
+			<div class="card border-primary" style="width: 18rem;">
+			  <img class="card-img-top" src=assets/images/pacitan.jpg alt="pacitan">
+			  <div class="card-body">
+			    <p class="card-text">Pacitan</p>
+			    <p class="card-text">-lebaran 2019</p>
+			  </div>
+			</div>
+		</div>
+	</div>
+</div> 
+</section>
+
+
+<section id="contact" class="contact">
+	<div class="container">
+		<div class="row pt-4 mb-4">
+			<div class="col text-center pt-4 mb-4">
+				<h2>Contact Me</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col">
+				<div class="card border-primary mb-5" style=";">
+				  <div class="card-body text-primary">
+				    <h3 class="card-title">Location</h3>
+				    <p class="card-text">Jl. Cendrawasih 6 RT 004/03 No. 107 Sawah Baru, Ciputat, Tangerang Selatan, 15413</p>
+				  </div>
+				</div>
+			</div>	
+		</div>
+		<div class="row">
+			<div class="col">
+				<div class="card border-primary mb-5" style=";">
+				  <div class="card-body text-primary">
+				    <h3 class="card-title" id="cp">CP</h3>
+				    <ul class="list-unstyled">
+				    <li><i class="material-icons">place</i> <span class="card-text">sabarfadhil@gmail.com</span></li>
+
+				    <li><i class="material-icons">&#xe0b0;</i> <span class="card-text">+62 896-0614-4800</span></li></ul>
+				  </div>
+				</div>
+			</div>	
+		</div>
+	</div>
+</section>
+
+
+
+<footer class="bg-dark text-dark">
+	<div class="container">
+		<div class="row pt-3">
+			<div class="col text-center">
+				<p>Made by Me</p>
+			</div>
+		</div>
+	</div>
+</footer>
